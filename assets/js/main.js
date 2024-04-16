c367f786f4a8cbd878434187923def5bb1838eda
ABCJIIN <97517088+ABCJIIN@users.noreply.github.com>
2024-04-17 00:26:41 +0900

    0417

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,38 +1,4 @@
 
-// 롤링배너
-// $(window).on('load', function(){
-//     window.scrollTo(0, 0); //새로고침시 페이지 최상단 이동
-//     setFlowBanner();
-// });
-
-// function setFlowBanner() {
-//     let bannerWrap = $('.flow-banner');
-//     let bannerList = $('.flow-banner .list');
-//     let bannerWrapWidth = bannerWrap.width();
-//     let bannerListWidth = bannerList.width();
-//     const speed = 90;//1초에 몇 픽셀 이동하는지 설정
-
-//     //리스트 복제
-//     let clone = bannerList.clone();
-//     bannerWrap.append(clone);
-//     flowBannerAct()
-
-//     //배너 실행 함수
-//     function flowBannerAct() {
-//         //무한 반복을 위해 리스트를 복제 후 배너에 추가
-//         if (bannerListWidth < bannerWrapWidth) {
-//             const listCount = Math.ceil(bannerWrapWidth * 2 / bannerListWidth);
-//             for(let i = 2; i < listCount; i++) {
-//                 clone = bannerList.clone();
-//                 bannerWrap.append(clone);
-//             }
-//         }
-//         bannerWrap.find('.list').css({
-//             'animation':`${bannerListWidth / speed}s linear infinite flowRolling`
-//         });
-//     }
-// };
-
 $(window).scroll(function(){
     if ($(this).scrollTop() >= 3000 && $(this).scrollTop() <= 3400) {
         animateGenerators($(".generator").first(), 1500);
